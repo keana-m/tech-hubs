586ae85983c04264248f03b26c61d3ea64973ed4
Keana Mabilog <keana-m@users.noreply.github.com>
2020-10-26 11:15:32 -0400

    changes tech job js to have drop down of job titles

--- static/js/tech_jobs.js
+++ static/js/tech_jobs.js
@@ -7,135 +7,159 @@ function unpack(rows, key) {
     return rows.map(function(row) { return row[key]; });
     }
 
-    var city = unpack(rows, 'area_title'),
-        a_median = unpack(rows, 'a_median'),
-        values = unpack(rows, 'tot_emp'),
-        job_title = unpack(rows, 'occ_title'),
-        a_pct10 = unpack(rows, 'a_pct10'),
-        a_pct25 = unpack(rows, 'a_pct25'),
-        a_pct75 = unpack(rows, 'a_pct75'),
-        a_pct90 = unpack(rows, 'a_pct90')
-        ;
+var allJobTitles = unpack(rows, 'occ_title'),
+    allCityx = unpack(rows, 'area_title'),
+    allWage10 = unpack(rows, 'a_pct10'),
+    allWage25 = unpack(rows, 'a_pct25'),
+    allWage75 = unpack(rows, 'a_pct75'),
+    allWage90 = unpack(rows, 'a_pct90'),
+    listofJobs = [],
+    currentCity = [],
+    currentWage10 = [],
+    currentWage25 = [],
+    currentWage75 = [],
+    currentWage90 = [];
 
-// Create Total Tech-Related Occupation Employment
-var trace2 = {
-    x: city,
-    y: a_pct10,
-    name: '10th percentile',
-    type: "bar",
-    text: a_pct10.map(String),
-    textposition: 'auto',
-    hoverinfo: 'on',
-    marker: {color: '#f0f8ff'},
-    transforms: [{
+  for (var i = 0; i < allJobTitles.length; i++ ){
+    if (listofJobs.indexOf(allJobTitles[i]) === -1 ){
+      listofJobs.push(allJobTitles[i]);
+    }
+  }
+  
+  function getJobData(chosenJob) {
+    currentCity = [];
+    currentWage10 = [],
+    currentWage25 = [],
+    currentWage75 = [],
+    currentWage90 = [];
+    for (var i = 0 ; i < allJobTitles.length ; i++){
+      if ( allJobTitles[i] === chosenJob ) {
+        currentCity.push(allCityx[i]);
+        currentWage10.push(allWage10[i]);
+        currentWage25.push(allWage25[i]);
+        currentWage75.push(allWage75[i]);
+        currentWage90.push(allWage90[i]);
+      } 
+    }
+  };
+
+// Default Country Data
+setBarChart('Actuaries');
+  
+function setBarChart(chosenJob) {
+  getJobData(chosenJob);  
+
+    var trace1 = {
+      x: currentCity,
+      y: currentWage10,
+      name: '10th percentile wage',
+      type: 'bar',
+      text: currentWage10.map(String),
+      textposition: 'auto',
+      hoverinfo: 'on',
+      marker: {color: '#f0f8ff'},
+      transforms: [{
         type: 'aggregate',
-        groups: city,
+        groups: currentCity,
         aggregations: [
           {target: 'y', func: 'avg', enabled: true},
         ]
       }]
     };
 
-var trace2a = {
-    x: city,
-    y: a_pct25,
-    name: '25th percentile',
-    type: "bar",
-    text: a_pct25.map(String),
-    textposition: 'auto',
-    hoverinfo: 'on',
-    marker: {color: '#b5ddff'},
-    transforms: [{
+    var trace2 = {
+      x: currentCity,
+      y: currentWage25,
+      name: '25th percentile wage',
+      type: 'bar',
+      text: currentWage25.map(String),
+      textposition: 'auto',
+      hoverinfo: 'on',
+      marker: {color: '#b5ddff'},
+      transforms: [{
         type: 'aggregate',
-        groups: city,
+        groups: currentCity,
         aggregations: [
           {target: 'y', func: 'avg', enabled: true},
         ]
       }]
     };
 
-var trace2b = {
-    x: city,
-    y: a_pct75,
-    name: '75th percentile',
-    type: "bar",
-    text: a_pct75.map(String),
-    textposition: 'auto',
-    hoverinfo: 'on',
-    marker: {color: '#7ac1ff'},
-    transforms: [{
+    var trace3 = {
+      x: currentCity,
+      y: currentWage75,
+      name: '70th percentile wage',
+      type: 'bar',
+      text: currentWage10.map(String),
+      textposition: 'auto',
+      hoverinfo: 'on',
+      marker: {color: '#7ac1ff'},
+      transforms: [{
         type: 'aggregate',
-        groups: city,
+        groups: currentCity,
         aggregations: [
           {target: 'y', func: 'avg', enabled: true},
         ]
       }]
     };
 
-var trace2c = {
-    x: city,
-    y: a_pct90,
-    name: '90th percentile',
-    type: "bar",
-    text: a_pct90.map(String),
-    textposition: 'auto',
-    hoverinfo: 'on',
-    marker: {color: '#3fa6ff'},
-    transforms: [{
+    var trace4 = {
+      x: currentCity,
+      y: currentWage90,
+      name: '90th percentile wage',
+      type: 'bar',
+      text: currentWage25.map(String),
+      textposition: 'auto',
+      hoverinfo: 'on',
+      marker: {color: '#3fa6ff'},
+      transforms: [{
         type: 'aggregate',
-        groups: city,
+        groups: currentCity,
         aggregations: [
           {target: 'y', func: 'avg', enabled: true},
         ]
       }]
-    
     };
 
-    
-var data2 = [trace2,trace2a,trace2b,trace2c];
+    var data = [trace1,trace2,trace3,trace4];
 
-Plotly.newPlot('plot3', data2, {
-    title: 'Tech Occupation Wages within Tech Hubs',
-    font: {
-      family: 'Arial',
-      size: 14,
-      color: '#00090c'
-    },
-    xaxis: {
-      tickson: "boundaries",
-      ticklen: 15,
-      showdividers: true,
-      dividercolor: 'grey',
-      dividerwidth: 2
-    },
-    showlegend: false,
-    yaxis: {range: [0, 200000]},
-    updatemenus: [{
-        y: 1,
-        yanchor: 'top',
-        buttons: [{
-            method: 'restyle',
-            args: ['visible', [true, true, true, true]],
-            label: 'All',
-        }, {
-            method: 'restyle',
-            args: ['visible', [true, false, false, false]],
-            label: '10th percentile'
-        }, {
-            method: 'restyle',
-            args: ['visible', [false, true, false, false]],
-            label: '25th percentile'
-        }, {
-            method: 'restyle',
-            args: ['visible', [false, false, true, false]],
-            label: '75th percentile'
-        }, {
-            method: 'restyle',
-            args: ['visible', [false, false, false, true]],
-            label: '90th percentile'
-        }]
-    }],
+    var layout = {
+      title: 'Wages for<br>'+ chosenJob + ' across Tech Hub Cities',
+      font: {
+        family: 'Arial',
+        size: 14,
+        color: '#00090c'
+      },
+      xaxis: {
+        tickson: "boundaries",
+        ticklen: 15,
+        showdividers: true,
+        dividercolor: 'grey',
+        dividerwidth: 2
+      },
+      yaxis: {range: [0, 200000]},
+    };
+
+    Plotly.newPlot('plotdiv', data, layout, {showSendToCloud: true});
+};
+  
+var innerContainer = document.querySelector('[data-num="0"'),
+    plotEl = innerContainer.querySelector('.plot'),
+    JobSelector = innerContainer.querySelector('.jobdata');
+
+function assignOptions(textArray, selector) {
+  for (var i = 0; i < textArray.length;  i++) {
+      var currentOption = document.createElement('option');
+      currentOption.text = textArray[i];
+      selector.appendChild(currentOption);
+  }
+}
+
+assignOptions(listofJobs, JobSelector);
+
+function updateJob(){
+  setBarChart(JobSelector.value);
+}
+  
+JobSelector.addEventListener('change', updateJob, false);
 });
-
-})
-	
